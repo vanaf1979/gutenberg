9f0d49e968a5d42288d1c924b21eaa96a80fef09
Jorge <jorge.costa@developer.pt>
2020-04-28 20:26:12 +0100

    Update: Widgets screen show disabled inserter when there is not widget area selected

--- packages/edit-widgets/src/components/inserter/index.js
+++ packages/edit-widgets/src/components/inserter/index.js
@@ -1,16 +1,38 @@
 /**
+ * External dependencies
+ */
+import { isEmpty } from 'lodash';
+
+/**
  * WordPress dependencies
  */
-import { createSlotFill } from '@wordpress/components';
+import { Slot, Fill } from '@wordpress/components';
+import { Inserter as BlockEditorInserter } from '@wordpress/block-editor';
 
-const { Fill: BlockInserterFill, Slot: BlockInserterSlot } = createSlotFill(
-	'EditWidgetsInserter'
-);
+const disabledInserterToggleProps = { isPrimary: true, disabled: true };
+
+function BlockInserterFill( props ) {
+	return <Fill name="EditWidgetsInserter" { ...props } />;
+}
+
+function BlockInserterSlot( props ) {
+	return (
+		<Slot name="EditWidgetsInserter" { ...props }>
+			{ ( fills ) => {
+				if ( ! isEmpty( fills ) ) {
+					return fills;
+				}
+				return (
+					<BlockEditorInserter
+						toggleProps={ disabledInserterToggleProps }
+					/>
+				);
+			} }
+		</Slot>
+	);
+}
 
 const Inserter = BlockInserterFill;
-
-Inserter.Slot = function() {
-	return <BlockInserterSlot bubblesVirtually />;
-};
+Inserter.Slot = BlockInserterSlot;
 
 export default Inserter;
